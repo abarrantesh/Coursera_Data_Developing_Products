c0a10301c9a2e30d4cc5ffc35e86310b3db568ce
Alexander Barrantes Herrera <abarrantesh@yahoo.com>
2020-04-27 00:30:59 -0600

    Add files via upload

---
+++ Week4/Shiny App/server.R
@@ -0,0 +1,50 @@
+# Name: Alexander Barrantes Herrera
+# Date: 26/04/2020
+
+# Course - Developing Data Products
+# Assignmet 3
+# Part I: Your Shiny Application (Server)
+
+
+library(shiny)
+library(datasets)
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+shinyServer(function(input, output) {
+  
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  formulaTextPoint <- reactive({
+    paste("mpg ~", "as.integer(", input$variable, ")")
+  })
+  
+  fit <- reactive({
+    lm(as.formula(formulaTextPoint()), data=mpgData)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  output$mpgBoxPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData,
+            outline = input$outliers)
+  })
+  
+  output$fit <- renderPrint({
+    summary(fit())
+  })
+  
+  output$mpgPlot <- renderPlot({
+    with(mpgData, {
+      plot(as.formula(formulaTextPoint()))
+      abline(fit(), col=2)
+    })
+  })
+  
+})
